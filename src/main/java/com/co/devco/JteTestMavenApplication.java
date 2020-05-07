a3ef96b1aed30b1a5c70936b47afff1c8119346c
Carlos Acevedo <cacevedod@devco.com.co>
2020-05-07 12:08:04 -0500

    Primer commit aun sin Jenkinsfile

---
+++ src/main/java/com/co/devco/JteTestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.co.devco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JteTestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JteTestMavenApplication.class, args);
+	}
+
+}
